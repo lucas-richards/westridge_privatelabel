862ef8f0734e05d6d4fac4796eebb41bcb7aebd4
lucas richards dev <lucasrichardsdev@gmail.com>
2024-03-14 13:09:31 -0700

    db

--- django_projects/settings.py
+++ django_projects/settings.py
@@ -36,7 +36,7 @@ DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'localhost',
-                 'https://westridgeapp.onrender.com/']
+                 'westridgeapp.onrender.com/']
 
 
 
