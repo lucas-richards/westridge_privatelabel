55d71c27768cabf23ce7253ed77eaa39ced45c22
lucas richards <lucasrichardsdev@gmail.com>
2024-08-15 19:56:54 +0000

    work orders records style

--- seed_workorders.py
+++ seed_workorders.py
@@ -2,15 +2,13 @@ import json
 import os
 import django
 from django.utils.dateparse import parse_datetime
+from django.utils import timezone
 
 # Set up Django environment
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_projects.settings')
 django.setup()
 
-from workorder.models import Asset, Location, Vendor
-from users.models import Department
-from django.contrib.auth.models import User
-from workorder.models import Asset, Location, Vendor
+from workorder.models import WorkOrder, Location, Vendor, Asset
 from users.models import Department
 from django.contrib.auth.models import User
 
@@ -23,14 +21,15 @@ django.setup()
 
 # Assume the JSON data is stored in a variable called `data`
 
-with open('./Assets_with_code.json') as f:
+with open('./work_orders_filtered_open_and_cleaned_up_final.json') as f:
     data = json.load(f)
 
-def load_assets(data):
+def load_workorders(data):
     for item in data:
-        location, _ = Location.objects.get_or_create(name=item['location'])
-        created_by = User.objects.get(first_name=item['created_by'].split()[0], last_name=item['created_by'].split()[1])
-        department_in_charge = Department.objects.get_or_create(name=item['department_in_charge'])[0] if item['department_in_charge'] else None
+        asset = Asset.objects.filter(code=item['code']).first()
+        created_by = User.objects.filter(first_name=item['Created by'].split()[0]).first()
+        assigned_to = User.objects.filter(first_name=item['Assigned to'].split()[0]).first() if item['Assigned to'] else None
+        department_in_charge = Department.objects.get_or_create(name=item['Teams Assigned to'])[0] if item['Teams Assigned to'] else None
         
         try:
             # Determine the image path
@@ -40,7 +39,6 @@ def load_assets(data):
                 image_files = os.listdir(image_path)
                 if image_files:  # Check if any files exist in the directory
                     first_image_file = image_files[0]  # Take the first file found
-                    print(first_image_file)
                     imageUrl = os.path.join('downloads', 'images', item['code'], first_image_file)
                     with open(os.path.join(image_path, first_image_file), 'rb') as f:
                         image = f.read()
@@ -53,44 +51,39 @@ def load_assets(data):
                 if attachment_files:  # Check if any files exist in the directory
                     first_attachment_file = attachment_files[0]  # Take the first file found
                     attachmentUrl = os.path.join('downloads', 'attachments', item['code'], first_attachment_file)
-                    print(attachmentUrl)
                     with open(os.path.join(attachments_path, first_attachment_file), 'rb') as f:
                         attachments = f.read()
             
-            # Create the Asset object
-            asset, created = Asset.objects.get_or_create(
-                code=item.get('code', ''),
-                name=item['name'],
-                status=item['status'],
-                parent=None,  # Assuming no parent data in the JSON, handle accordingly if it exists
-                location=location,
-                description=item.get('description', ''),
-                serial_number=item.get('serial_number', ''),
-                model=item.get('model', ''),
-                manufacturer=item.get('manufacturer', ''),
-                year=item.get('year', None),
-                department_in_charge=department_in_charge,
+            # Create the workorder object
+            workorder, created = WorkOrder.objects.get_or_create(
+                title=item['Title'],
+                description=item.get('Description', ''),
+                priority=item.get('Priority', 'medium').lower(),
+                assigned_to=assigned_to,
+                department_assigned_to=department_in_charge,
                 created_by=created_by,
-                created_on=parse_datetime(item['created_on']),
-                last_updated=parse_datetime(item['last_updated']),
-                image=imageUrl if 'imageUrl' in locals() else '',
-                attachments=attachmentUrl if 'attachments' in locals() else '',
+                # created_on=parse_datetime(item['created_on'], timezone.now()),
+                # first_due_date=parse_datetime(item['Due Date']),
+                recurrence=item.get('recurrence', 'once').lower(),
+                asset=asset if asset else None,
+                # image=image,
+                # attachments=attachments,
             )
             
             if created:
-                print(f"Asset {item['name']} created successfully")
+                print(f"WorkOrder {item['Title']} created successfully")
             else:
                 # update attachment
                 if attachmentUrl:
-                    asset.attachments = attachmentUrl
-                    asset.save()
+                    workorder.attachments = attachmentUrl
+                    workorder.save()
 
-                print(f"Asset {item['name']} updated successfully")
+                print(f"workorder {item['Title']} updated successfully")
             
         except Exception as e:
-            print(f"Asset with code {item['code']} encountered a problem")
-            print(f"Error while creating asset: {e}")
+            print(f"workorder with code {item['Title']} encountered a problem")
+            print(f"Error while creating workorder: {e}")
 
 if __name__ == "__main__":
-    load_assets(data)
+    load_workorders(data)
 
