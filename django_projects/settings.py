4dc07ca79713bf455322541d568f2620d7eb80d4
lucas richards <lucasrichardsdev@gmail.com>
2025-04-28 17:33:15 -0700

    update pl

--- django_projects/settings.py
+++ django_projects/settings.py
@@ -193,7 +193,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
-LOGIN_REDIRECT_URL = 'privatelabel-dashboard'
+LOGIN_REDIRECT_URL = '/privatelabel/orders/'
 LOGIN_URL = 'get-code'
 
 #variables to send email and reset password
